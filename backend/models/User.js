62814f7d174095b86fdb26d75cec22663dedc920
Clara Hlt <Clarahltc@gmail.com>
2021-09-20 16:46:00 +0200

    validation champ email signup/login: installation plugin mongoose-type-email + installation mongoose-sanitizer

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+require = ('mongoose-type-email');
+const sanitizerPlugin = require('mongoose-sanitizer');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true},
+  email: { type: String, required: [true, "Veuillez entrer votre adresse email"], unique: true,
+  match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]},
   password: { type: String, required: true}
 });
 
 userSchema.plugin(uniqueValidator);
+userSchema.plugin(sanitizerPlugin);
 
 module.exports = mongoose.model('User', userSchema);
